eb3a684cdc309e6c17a6ec55b24355e6958d7b90
Claudia Petty <claudialove32@gmail.com>
2020-06-15 18:53:59 -0400

    while loop but with strings not numbers

---
+++ script.js
@@ -0,0 +1,12 @@
+//be able to ask user
+var ask = prompt("Are we there yet??");
+
+
+//while loop condition...
+while (ask !== "yes" && ask !== "yeah") {
+    var ask = prompt("Are we there yet??");
+};
+
+alert("YAY!!!! WE MADE IT!!!");
+
+
